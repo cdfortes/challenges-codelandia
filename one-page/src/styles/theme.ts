2ebaf5e194a9fb64fd9393f2503ed02fc55b9d67
cdfortes <cdfortes@gmail.com>
2022-10-17 11:10:14 -0100

    project structured

---
+++ one-page/src/styles/theme.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  white: '#FFF',
+  'white-200': '#F9F9F9',
+
+  'gray-400': '#555555',
+  'gray-900': '#2B2B2B',
+
+  purple: '#6C63FF',
+  'purple-light': '#D7D4FF',
+
+  'body-family': 'Lato, sans-serif;',
+
+  'heading-family': "'Merriweather', serif;",
+
+  'font-sizes': {
+    sm: '1.8rem',
+    md: '1.9rem',
+    md2: '2.0rem',
+    xd: '2.4rem',
+    xl: '3.6rem'
+  },
+
+  'container-width': '124.2rem'
+}
